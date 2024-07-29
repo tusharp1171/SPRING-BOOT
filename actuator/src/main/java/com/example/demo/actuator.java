31a041b85ca58c48d32a1a6b3cdbfb81ec786a01
tusharp1171 <tusharpatil11710@gmail.com>
2024-07-29 09:35:50 +0530

    actuator code

---
+++ actuator/src/main/java/com/example/demo/actuator.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class actuator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(actuator.class, args);
+	}
+
+}
